d79d01a8295f2b6dcc23c66ddef15cff282ec683
daBard <bard.johan@gmail.com>
2024-09-17 16:37:58 +0200

    Completed

---
+++ ecu-onatrix-emailProvider/Functions/EmailSender.cs
@@ -0,0 +1,77 @@
+using Azure;
+using Azure.Communication.Email;
+using Azure.Messaging.ServiceBus;
+using ecu_onatrix_emailProvider.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ecu_onatrix_emailProvider.Functions;
+
+public class EmailSender(ILogger<EmailSender> logger, EmailClient emailClient)
+{
+    private readonly ILogger<EmailSender> _logger = logger;
+    private readonly EmailClient _emailClient = emailClient;
+
+    [Function(nameof(EmailSender))]
+    public async Task Run(
+        [ServiceBusTrigger("email_request", Connection = "ServiceBusConnection")]
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        try
+        {
+            var emailRequest = UnpackEmailRequest(message);
+            if (emailRequest != null && !string.IsNullOrEmpty(emailRequest.To))
+            {
+                if(SendEmail(emailRequest))
+                {
+                    await messageActions.CompleteMessageAsync(message);
+                }
+            }    
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : EmailSender.Run :: {ex.Message}");
+        }
+    }
+
+    public EmailRequest UnpackEmailRequest(ServiceBusReceivedMessage message)
+    {
+        try
+        {
+            var emailRequest = JsonConvert.DeserializeObject<EmailRequest>(message.Body.ToString());
+            if (emailRequest != null) 
+                return emailRequest;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : EmailSender.UnpackEmailRequest() :: {ex.Message}");
+        }
+
+        return null!;
+    }
+
+    public bool SendEmail(EmailRequest emailRequest)
+    {
+        try
+        {
+            var result = _emailClient.Send(
+                WaitUntil.Completed,
+                senderAddress: Environment.GetEnvironmentVariable("SenderAddress"),
+                recipientAddress: emailRequest.To,
+                subject: emailRequest.Subject,
+                htmlContent: emailRequest.HtmlContent,
+                plainTextContent: emailRequest.PlainTextContent);
+
+            if (result.HasCompleted)
+                return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : EmailSender.SendEmailAsync() :: {ex.Message}");
+        }
+
+        return false;
+    }
+}
---
+++ ecu-onatrix-emailProvider/Models/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ecu_onatrix_emailProvider.Models;
+
+public class EmailRequest
+{
+    public string To { get; set; } = null!;
+    public string Subject { get; set; } = null!;
+    public string HtmlContent { get; set; } = null!;
+    public string PlainTextContent {  get; set; } = null!;
+}
