1c20e06dac1f5e99b0f96eee554e39f76192e3f3
daBard <50872455+daBard@users.noreply.github.com>
2024-09-20 11:04:11 +0200

    bugfix EmailRequest returned string, not null

--- ecu-onatrix-emailProvider/Models/EmailRequest.cs
+++ ecu-onatrix-emailProvider/Models/EmailRequest.cs
@@ -3,7 +3,7 @@
 public class EmailRequest
 {
     public string To { get; set; } = null!;
-    public string Subject { get; set; } = "null!";
-    public string HtmlContent { get; set; } = "null!";
-    public string PlainTextContent {  get; set; } = "null!";
+    public string Subject { get; set; } = null!;
+    public string HtmlContent { get; set; } = null!;
+    public string PlainTextContent {  get; set; } = null!;
 }
